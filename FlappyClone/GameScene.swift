a68686bd81412fe7a3fa1bf1ea60949772e313c9
linwei365 <linwei365@gmail.com>
2016-02-05 05:31:36 -0800

    added spawn pipes and set dragon rotation set to false

--- FlappyClone/GameScene.swift
+++ FlappyClone/GameScene.swift
@@ -12,7 +12,7 @@ import SpriteKit
 struct PhysicsCatagory {
     
     // bitwise left shift operator (<<). so "<< 1" shifts to the right one unit in bitwise
-    static let Ghost : UInt32 = 0x1 << 1
+    static let dragon : UInt32 = 0x1 << 1
     static let Ground : UInt32 = 0x1 << 2
     static let Wall : UInt32 = 0x1 << 3
 }
@@ -22,12 +22,16 @@ struct PhysicsCatagory {
 class GameScene: SKScene {
   
     var ground:SKSpriteNode?
-    var ghost: SKSpriteNode?
-    
+    var dragon: SKSpriteNode?
+    var wallPair = SKNode()
  
     
     var imageArrayB = [SKTexture]()
     var animAction = SKAction()
+    
+    var  moveAndRemove =  SKAction ()
+    var gameStarted = Bool()
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         
@@ -45,17 +49,18 @@ class GameScene: SKScene {
         //adding dynamic
         ground?.physicsBody = SKPhysicsBody(rectangleOfSize: (ground?.size)!)
         ground?.physicsBody?.categoryBitMask = PhysicsCatagory.Ground
-        ground?.physicsBody?.collisionBitMask = PhysicsCatagory.Ghost
-        ground?.physicsBody?.contactTestBitMask = PhysicsCatagory.Ghost
+        ground?.physicsBody?.collisionBitMask = PhysicsCatagory.dragon
+        ground?.physicsBody?.contactTestBitMask = PhysicsCatagory.dragon
         ground?.physicsBody?.affectedByGravity = false
         ground?.physicsBody?.dynamic = false
         ground?.zPosition = 3
         
         self.addChild(ground!)
-        
+       
+        //load dragon sequence
         for i in 1...9 {
             
-            let imageName = "dragon\(i)"
+        let imageName = "dragon\(i)"
            
             imageArrayB += [SKTexture(imageNamed: imageName)]
           animAction = SKAction.animateWithTextures(imageArrayB, timePerFrame: 0.1)
@@ -63,30 +68,51 @@ class GameScene: SKScene {
         }
         
         
-        ghost = SKSpriteNode(imageNamed:"dragon1")
+        dragon = SKSpriteNode(imageNamed:"dragon1")
        
        let action = SKAction.repeatActionForever(animAction)
         
-        ghost?.runAction(action)
+        dragon?.runAction(action)
         
        
-        ghost?.size = CGSize (width: 140, height: 120)
-        ghost?.position = CGPoint(x: self.frame.width/2 - ghost!.frame.width+30, y: self.frame.height/2)
+        dragon?.size = CGSize (width: 100, height: 100)
+        dragon?.position = CGPoint(x: self.frame.width/2 , y: self.frame.height/2)
         
-        //adding physicsBody to ghost
-        ghost?.physicsBody = SKPhysicsBody(circleOfRadius: ((ghost?.frame.height)!/2))
-        ghost?.physicsBody?.categoryBitMask = PhysicsCatagory.Ghost
-        ghost?.physicsBody?.collisionBitMask = PhysicsCatagory.Ground | PhysicsCatagory.Wall
-        ghost?.physicsBody?.contactTestBitMask = PhysicsCatagory.Ground | PhysicsCatagory.Wall
-        ghost?.physicsBody?.affectedByGravity = true
-        ghost?.physicsBody?.dynamic = true
+        //adding physicsBody to dragon
+        dragon?.physicsBody = SKPhysicsBody(circleOfRadius: ((dragon?.frame.height)!/3))
         
-        ghost?.zPosition = 2
-        self.addChild(ghost!)
+        dragon?.physicsBody?.categoryBitMask = PhysicsCatagory.dragon
+        dragon?.physicsBody?.collisionBitMask = PhysicsCatagory.Ground | PhysicsCatagory.Wall
+        dragon?.physicsBody?.contactTestBitMask = PhysicsCatagory.Ground | PhysicsCatagory.Wall
+        dragon?.physicsBody?.affectedByGravity = true
+        dragon?.physicsBody?.allowsRotation = false
+        dragon?.physicsBody?.dynamic = true
+        
+        dragon?.zPosition = 2
+        self.addChild(dragon!)
+        
+        
+        
         
         
         //create wall
-        createWall()
+        
+        let wallSpawn =  SKAction.runBlock { () -> Void in
+            self.createWall()
+        }
+        
+        let delay = SKAction.waitForDuration(2.0)
+        let wallSpawnDelay = SKAction.sequence([wallSpawn, delay])
+        let spawnActionForever = SKAction.repeatActionForever(wallSpawnDelay)
+        self.runAction(spawnActionForever)
+   
+      let distance = CGFloat(self.frame.width + wallPair.frame.width)
+        
+        let movePipes = SKAction.moveByX(-distance, y: 0, duration: NSTimeInterval(0.01 * distance))
+        let removePipes = SKAction.removeFromParent()
+        
+        moveAndRemove = SKAction.sequence([movePipes,removePipes])
+       
         
         
         
@@ -95,19 +121,19 @@ class GameScene: SKScene {
     
     func createWall (){
         
-        let wallPair:SKNode = SKNode()
+         wallPair = SKNode()
         
         let topWall: SKSpriteNode = SKSpriteNode(imageNamed: "Wall")
         topWall.setScale(0.5)
-        topWall.position = CGPoint(x: self.frame.width/2, y: self.frame.height/2 + 380)
+        topWall.position = CGPoint(x: self.frame.width, y: self.frame.height/2 + 350)
         //rotate the the topWall in z 180 degree radian
         topWall.zRotation = CGFloat(M_PI)
         
         //adding physics to topWall
         topWall.physicsBody = SKPhysicsBody(rectangleOfSize: topWall.size)
         topWall.physicsBody?.categoryBitMask = PhysicsCatagory.Wall
-        topWall.physicsBody?.collisionBitMask = PhysicsCatagory.Ghost
-        topWall.physicsBody?.contactTestBitMask = PhysicsCatagory.Ghost
+        topWall.physicsBody?.collisionBitMask = PhysicsCatagory.dragon
+        topWall.physicsBody?.contactTestBitMask = PhysicsCatagory.dragon
         topWall.physicsBody?.affectedByGravity = false
         topWall.physicsBody?.dynamic = false
 
@@ -118,18 +144,21 @@ class GameScene: SKScene {
         
         let buttomWall: SKSpriteNode = SKSpriteNode(imageNamed: "Wall")
         buttomWall.setScale(0.5)
-        buttomWall.position = CGPoint(x: self.frame.width/2, y: self.frame.height/2 - 380)
+        buttomWall.position = CGPoint(x: self.frame.width, y: self.frame.height/2 - 350)
         wallPair.addChild(buttomWall)
         
         //adding physics to buttomWall
         buttomWall.physicsBody = SKPhysicsBody(rectangleOfSize: buttomWall.size)
         buttomWall.physicsBody?.categoryBitMask = PhysicsCatagory.Wall
-        buttomWall.physicsBody?.collisionBitMask = PhysicsCatagory.Ghost
-        buttomWall.physicsBody?.contactTestBitMask = PhysicsCatagory.Ghost
+        buttomWall.physicsBody?.collisionBitMask = PhysicsCatagory.dragon
+        buttomWall.physicsBody?.contactTestBitMask = PhysicsCatagory.dragon
         buttomWall.physicsBody?.affectedByGravity = false
         buttomWall.physicsBody?.dynamic = false
         
         wallPair.zPosition = 1
+        
+        wallPair.runAction(moveAndRemove)
+        
         self.addChild(wallPair)
         
     }
@@ -140,8 +169,8 @@ class GameScene: SKScene {
        /* Called when a touch begins */
 
         
-        ghost?.physicsBody?.velocity = CGVectorMake(0, 0)
-        ghost?.physicsBody?.applyImpulse(CGVectorMake(0, 260))
+        dragon?.physicsBody?.velocity = CGVectorMake(0, 0)
+        dragon?.physicsBody?.applyImpulse(CGVectorMake(0, 90))
         
     
     }
