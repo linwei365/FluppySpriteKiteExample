871dc4de4f7a9a27c95dadc5f793c744922d4a18
linwei365 <linwei365@gmail.com>
2016-02-08 02:20:25 -0800

    added sound effect when flappying, wind bg sound, drg dying sound, added clouds

--- FlappyClone/GameScene.swift
+++ FlappyClone/GameScene.swift
@@ -7,6 +7,7 @@
 //
 
 import SpriteKit
+import AVFoundation
 
 
 struct PhysicsCatagory {
@@ -14,7 +15,7 @@ struct PhysicsCatagory {
     // bitwise left shift operator (<<). so "<< 1" shifts to the right one unit in bitwise
     static let dragon : UInt32 = 0x1 << 1
     static let Ground : UInt32 = 0x1 << 2
-    static let Wall : UInt32 = 0x1 << 3
+    static let Cloud : UInt32 = 0x1 << 3
     static let Score : UInt32 = 0x1 << 4
 }
 
@@ -24,8 +25,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
   
     var ground:SKSpriteNode?
     var dragon: SKSpriteNode?
-    var wallPair = SKNode()
+    var CloudPair = SKNode()
  
+    
+    var imageName = String()
+    var imageNameB = String ()
     
     var imageArrayB = [SKTexture]()
     var animAction = SKAction()
@@ -37,11 +41,24 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var score = Int()
     
     var touchCount =  Int()
-    
+    var touchGestureCount = Int()
     var dieState = Bool()
     
     var restartButton = SKSpriteNode()
+    var dragonPhyscialbody = SKPhysicsBody()
     
+   
+    
+    // Grab the path, make sure to add it to your project!
+    var windSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("WindSoundEffectLoop", ofType: "mp3")!)
+    var flappingWingSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("FlappyingWings", ofType: "mp3")!)
+    var dragonDyingSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("dragonDying", ofType: "mp3")!)
+
+    
+    var audioPlayer = AVAudioPlayer()
+    var aduioPlayerB = AVAudioPlayer()
+    var audioPlayerC = AVAudioPlayer()
+  
     
     func restartGame(){
    
@@ -51,12 +68,55 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         gameStarted = false
         score = 0
         touchCount = 0
-   
+        touchGestureCount = 0
         createScene()
         
     }
+    func loadDragonSequence() {
+        
+        //load dragon sequence
+        for i in 1...9 {
+            
+            imageName = "dragon\(i)"
+            imageNameB = "dragonFlame\(i)"
+            
+            if touchGestureCount%4 == 1 {
+                imageArrayB += [SKTexture(imageNamed: imageNameB)]
+            }
+            else {
+                imageArrayB += [SKTexture(imageNamed: imageName)]
+            }
+            
+            
+            animAction = SKAction.animateWithTextures(imageArrayB, timePerFrame: 0.1)
+            
+            /* unused physicsBody on alpha Channel
+            dragonPhyscialbody =  SKPhysicsBody(texture: SKTexture(imageNamed: "dragon1"), alphaThreshold: 1, size: (dragon?.size)!)
+            */
+        }
+    }
     
     func createScene (){
+        
+   
+       // audio player
+        do {
+            try        audioPlayer = AVAudioPlayer(contentsOfURL: windSound, fileTypeHint: nil)
+            try aduioPlayerB =  AVAudioPlayer(contentsOfURL: flappingWingSound, fileTypeHint: nil)
+            try audioPlayerC = AVAudioPlayer(contentsOfURL: dragonDyingSound, fileTypeHint: nil)
+         }
+        catch{
+            
+            print("audio error")
+        }
+
+        audioPlayer.prepareToPlay()
+         audioPlayer.numberOfLoops = -1
+        audioPlayer.play()
+        
+       
+        
+        
         
         //score label
         scoreLabel.position = CGPoint(x: self.frame.width/2, y: self.frame.height/2 + self.frame.height / 2.5)
@@ -91,15 +151,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         self.addChild(ground!)
         
-        //load dragon sequence
-        for i in 1...9 {
-            
-            let imageName = "dragon\(i)"
-            
-            imageArrayB += [SKTexture(imageNamed: imageName)]
-            animAction = SKAction.animateWithTextures(imageArrayB, timePerFrame: 0.1)
-            
-        }
+ 
+        //load image sequence
+        loadDragonSequence()
         
         
         dragon = SKSpriteNode(imageNamed:"dragon1")
@@ -108,16 +162,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         dragon?.runAction(action)
         
-        
+       
         dragon?.size = CGSize (width: 100, height: 100)
+        
+      
         dragon?.position = CGPoint(x: self.frame.width/2 , y: self.frame.height/2)
         
         //adding physicsBody to dragon
-        dragon?.physicsBody = SKPhysicsBody(circleOfRadius: ((dragon?.frame.height)!/4))
+    /*
+        dragon?.physicsBody = dragonPhyscialbody
+    */
+       dragon?.physicsBody = SKPhysicsBody(circleOfRadius: ((dragon?.frame.height)!/4))
         
         dragon?.physicsBody?.categoryBitMask = PhysicsCatagory.dragon
-        dragon?.physicsBody?.collisionBitMask = PhysicsCatagory.Ground | PhysicsCatagory.Wall
-        dragon?.physicsBody?.contactTestBitMask = PhysicsCatagory.Ground | PhysicsCatagory.Wall | PhysicsCatagory.Score
+        dragon?.physicsBody?.collisionBitMask = PhysicsCatagory.Ground | PhysicsCatagory.Cloud
+        dragon?.physicsBody?.contactTestBitMask = PhysicsCatagory.Ground | PhysicsCatagory.Cloud | PhysicsCatagory.Score
         dragon?.physicsBody?.affectedByGravity = false
 //        dragon?.physicsBody?.allowsRotation = false
         dragon?.physicsBody?.dynamic = true
@@ -148,8 +207,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
     }
     func didBeginContact(contact: SKPhysicsContact) {
+        
+        
+        
         let contactBodyA = contact.bodyA
         let contactBodyB = contact.bodyB
+        
+        
+        
         if contactBodyA.categoryBitMask == PhysicsCatagory.Score && contactBodyB.categoryBitMask == PhysicsCatagory.dragon || contactBodyA.categoryBitMask == PhysicsCatagory.dragon && contactBodyB.categoryBitMask == PhysicsCatagory.Score {
             
            score++
@@ -160,22 +225,25 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             
         }
      
-        if contactBodyA.categoryBitMask == PhysicsCatagory.Wall && contactBodyB.categoryBitMask == PhysicsCatagory.dragon || contactBodyA.categoryBitMask == PhysicsCatagory.dragon && contactBodyB.categoryBitMask == PhysicsCatagory.Wall {
+        if contactBodyA.categoryBitMask == PhysicsCatagory.Cloud && contactBodyB.categoryBitMask == PhysicsCatagory.dragon || contactBodyA.categoryBitMask == PhysicsCatagory.dragon && contactBodyB.categoryBitMask == PhysicsCatagory.Cloud {
     
             dieState = true
             
+        
             
             touchCount++
             if touchCount == 1 {
                 print("touched")
          
-//          dragon?.physicsBody?.collisionBitMask = PhysicsCatagory.Ground
+          //if dragon got hit make sound
+                audioPlayerC.prepareToPlay()
+                audioPlayerC.play()
+                
             createButton()
-//
                 
             }
             print(touchCount)
-            enumerateChildNodesWithName("wallPair", usingBlock: { (node, error ) -> Void in
+            enumerateChildNodesWithName("CloudPair", usingBlock: { (node, error ) -> Void in
                 node.speed = 0
                 self.removeAllActions()
             })
@@ -185,12 +253,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
     }
     
-    func createWall (){
+    func createCloud (){
         
         //add score node
         let scoreNode = SKSpriteNode()
-        scoreNode.size = CGSize(width: 1, height: 200)
+        scoreNode.size = CGSize(width: 1, height: 220)
         scoreNode.position = CGPoint(x: self.frame.width, y: self.frame.height/2)
+        
         scoreNode.physicsBody = SKPhysicsBody(rectangleOfSize: scoreNode.size)
         scoreNode.physicsBody?.affectedByGravity = false
         scoreNode.physicsBody?.dynamic = false
@@ -202,80 +271,90 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         
         //----
-         wallPair = SKNode()
-        wallPair.addChild(scoreNode)
-        wallPair.name = "wallPair"
+         CloudPair = SKNode()
+        CloudPair.addChild(scoreNode)
+        CloudPair.name = "CloudPair"
         
-        let topWall: SKSpriteNode = SKSpriteNode(imageNamed: "Wall")
-        topWall.setScale(0.5)
-        topWall.position = CGPoint(x: self.frame.width, y: self.frame.height/2 + 350)
-        //rotate the the topWall in z 180 degree radian
-        topWall.zRotation = CGFloat(M_PI)
         
-        //adding physics to topWall
-        topWall.physicsBody = SKPhysicsBody(rectangleOfSize: topWall.size)
-        topWall.physicsBody?.categoryBitMask = PhysicsCatagory.Wall
-        topWall.physicsBody?.collisionBitMask = PhysicsCatagory.dragon
-        topWall.physicsBody?.contactTestBitMask = PhysicsCatagory.dragon
-        topWall.physicsBody?.affectedByGravity = false
-        topWall.physicsBody?.dynamic = false
+        let topCloud: SKSpriteNode = SKSpriteNode(imageNamed: "Cloud")
+        topCloud.setScale(0.5)
+        topCloud.position = CGPoint(x: self.frame.width, y: self.frame.height/2 + 360)
+        //rotate the the topCloud in z 180 degree radian
+        topCloud.zRotation = CGFloat(M_PI)
+        
+        //adding physics to topCloud
+        topCloud.physicsBody = SKPhysicsBody(texture: SKTexture(imageNamed: "Cloud"), alphaThreshold: 0.5, size: (topCloud.size))
+        
+//        topCloud.physicsBody = SKPhysicsBody(rectangleOfSize: topCloud.size)
+        topCloud.physicsBody?.categoryBitMask = PhysicsCatagory.Cloud
+        topCloud.physicsBody?.collisionBitMask = PhysicsCatagory.dragon
+        topCloud.physicsBody?.contactTestBitMask = PhysicsCatagory.dragon
+        topCloud.physicsBody?.affectedByGravity = false
+        topCloud.physicsBody?.dynamic = false
 
         
-        wallPair.addChild(topWall)
+        CloudPair.addChild(topCloud)
         
         
 
         
-        let buttomWall: SKSpriteNode = SKSpriteNode(imageNamed: "Wall")
-        buttomWall.setScale(0.5)
-        buttomWall.position = CGPoint(x: self.frame.width, y: self.frame.height/2 - 350)
-        wallPair.addChild(buttomWall)
+        let buttomCloud: SKSpriteNode = SKSpriteNode(imageNamed: "Cloud")
+        buttomCloud.setScale(0.5)
+        buttomCloud.position = CGPoint(x: self.frame.width, y: self.frame.height/2 - 360)
+        CloudPair.addChild(buttomCloud)
         
-        //adding physics to buttomWall
-        buttomWall.physicsBody = SKPhysicsBody(rectangleOfSize: buttomWall.size)
-        buttomWall.physicsBody?.categoryBitMask = PhysicsCatagory.Wall
-        buttomWall.physicsBody?.collisionBitMask = PhysicsCatagory.dragon
-        buttomWall.physicsBody?.contactTestBitMask = PhysicsCatagory.dragon
-        buttomWall.physicsBody?.affectedByGravity = false
-        buttomWall.physicsBody?.dynamic = false
+        //adding physics to buttomCloud
+        buttomCloud.physicsBody = SKPhysicsBody(texture: SKTexture(imageNamed: "Cloud"), alphaThreshold: 0.5, size: (buttomCloud.size))
+//        buttomCloud.physicsBody = SKPhysicsBody(rectangleOfSize: buttomCloud.size)
+        buttomCloud.physicsBody?.categoryBitMask = PhysicsCatagory.Cloud
+        buttomCloud.physicsBody?.collisionBitMask = PhysicsCatagory.dragon
+        buttomCloud.physicsBody?.contactTestBitMask = PhysicsCatagory.dragon
+        buttomCloud.physicsBody?.affectedByGravity = false
+        buttomCloud.physicsBody?.dynamic = false
         
-        wallPair.zPosition = 1
+        CloudPair.zPosition = 1
         
         let randomPosition = CGFloat.random(-200, max: 200)
         
-        wallPair.position.y += randomPosition
+        CloudPair.position.y += randomPosition
         
         
-        wallPair.runAction(moveAndRemove)
+        CloudPair.runAction(moveAndRemove)
         
         
         
-        self.addChild(wallPair)
+        self.addChild(CloudPair)
         
     }
     
     
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
+         touchGestureCount++
+         aduioPlayerB.prepareToPlay()
+        aduioPlayerB.play() 
+        
+        print(touchGestureCount)
+        /* Called when a touch begins */
 
         if gameStarted == false {
             
             gameStarted = true
             
             dragon?.physicsBody?.affectedByGravity = true
-            //create wall
+            //create Cloud
             
-            let wallSpawn =  SKAction.runBlock { () -> Void in
-                self.createWall()
+            let CloudSpawn =  SKAction.runBlock { () -> Void in
+                self.createCloud()
             }
             
             let delay = SKAction.waitForDuration(2.0)
-            let wallSpawnDelay = SKAction.sequence([wallSpawn, delay])
-            let spawnActionForever = SKAction.repeatActionForever(wallSpawnDelay)
+            let CloudSpawnDelay = SKAction.sequence([CloudSpawn, delay])
+            let spawnActionForever = SKAction.repeatActionForever(CloudSpawnDelay)
             self.runAction(spawnActionForever)
             
-            let distance = CGFloat(self.frame.width + wallPair.frame.width)
+            let distance = CGFloat(self.frame.width + CloudPair.frame.width )
+           
             
             let movePipes = SKAction.moveByX(-distance, y: 0, duration: NSTimeInterval(0.01 * distance))
             let removePipes = SKAction.removeFromParent()
